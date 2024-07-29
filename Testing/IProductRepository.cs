7084007c80dd94ce9b333e81965a64b65a8c153b
“Noah <urbannoah@yahoo.com>
2024-07-29 10:02:39 -0400

    updated appsettings

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+    }
+}
+
